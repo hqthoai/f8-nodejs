20ef8a729123cef06cb3d00dfe07f0cec189a733
Quoc Thoai <thoaifamily83@gmail.com>
2023-06-07 02:31:04 +0700

    boostrap

--- index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000
-app.get('/', (req,res) => res.send("NodeJS!!!"))
-
-app.listen(port, ()=> console.log(`Running at http://localhost:${port}`));

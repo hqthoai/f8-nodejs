e109ec4d699be506fd85c009169fbb890481c07c
Quoc Thoai <thoaifamily83@gmail.com>
2023-06-06 22:54:36 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 3000
+app.get('/', (req,res) => res.send("NodeJS!!!"))
+
+app.listen(port, ()=> console.log(`Running at http://localhost:${port}`));
